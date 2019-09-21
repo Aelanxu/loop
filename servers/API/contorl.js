737643ce42aa303d36764b88efa650dd657bbc9f
Aelanxu <aelanxu@outlook.com>
2019-09-21 22:00:57 +0800

    222

--- servers/API/contorl.js
+++ servers/API/contorl.js
@@ -1,49 +1,53 @@
 import * as querystring from 'querystring';
-import {cData} from '../data/data.js';
-import {getD} from '../getdata.js';
+import { cData } from '../data/data.js';
+import { getD } from '../getdata.js';
 
 
-function login(req,res){
-        getD.POST(req,res).then((data)=>{
-               console.log(querystring.parse(data));
+function login(req, res) {
+    getD.POST(req, res).then((data) => {
+        console.log(querystring.parse(data));
 
-               cData.add('users',querystring.parse(data)).then(result=>{console.log(result)})
-               
+        cData.add('users', querystring.parse(data)).then(result => {
+            console.log(result.insertedId.toHexString()) //获取返回的ID
+            let timestamp = Date.now();
+            console.log(new Date(timestamp))
         })
 
-            
+    })
 
-}        
+
+
+}
 //     let postData="";
 //     req.setEncoding('utf-8')
-  
+
 //     req.on('data',function(chunk){
 //            postData+=chunk;
 //     })
 //     req.on('end',function(){
 //            if(postData!==""){
-              
+
 //             postData=querystring.parse(postData);
 //             postData=JSON.stringify(postData);
 //             res.writeHead(200, { "Content-Type": "application/json" });
-           
-           
+
+
 //             cData.find('jsondata').then((data)=>{
 //                    console.log(data);
 //                    res.end(JSON.stringify(data));
 //             })
 //             //cData.add('jsondata',JSON.parse(postData))
-         
-           
+
+
 //            }else{
 //             let mes={mes:'nodata'}
 //             res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
 //             res.write(mes.mes);
 //             res.end();
 //            }
-           
-           
-//     })
-  
 
-export {login};
+
+//     })
+
+
+export { login };
--- servers/data/data.js
+++ servers/data/data.js
@@ -1,82 +1,82 @@
 //封装数据操作类9/17
 import * as mongodb from 'mongodb';
-import {dataConfig} from'../config/config.js';//read the config for data
+import { dataConfig } from '../config/config.js'; //read the config for data
 //create the MongoClient of mongodb 
-const MongoClient=mongodb.default.MongoClient;
+const MongoClient = mongodb.default.MongoClient;
 //check username
-let state =null;
-if(dataConfig.username!=''&& dataConfig.password!=''){
-    state=true;
-}else{
-    state=false;
+let state = null;
+if (dataConfig.username != '' && dataConfig.password != '') {
+    state = true;
+} else {
+    state = false;
 }
 //console.log(state)
 // define data class
- class cData{
-     //share the stance
-     static getInstance(){
-         if(!cData.instance){
-             cData.instance =new cData()
-         }
-         return cData.instance;
-     };
+class cData {
+    //share the stance
+    static getInstance() {
+        if (!cData.instance) {
+            cData.instance = new cData()
+        }
+        return cData.instance;
+    };
 
-     //dafault for  methods
-     constructor(){
-         // set mongodb connect object
-        this.dbClient='';
+    //dafault for  methods
+    constructor() {
+        // set mongodb connect object
+        this.dbClient = '';
         //init connect database
         this.connect();
-     };
+    };
 
-    connect(){
-        if(state){
-            return new Promise((resolve,reject)=>{
-                if(!this.dbClient){
-                    MongoClient.connect(`mongodb://${dataConfig.username}:${dataConfig.password}@${dataConfig.address}:${dataConfig.port}/${dataConfig.database}`,{useNewUrlParser: true,useUnifiedTopology: true},
-                    (err,client)=>{
-                        if(!err){
-                            this.dbClient=client.db(dataConfig.database);
-                            resolve(this.dbClient);
-                            console.log('usename: connect is  success!')
-                        }else{
-                            reject(err);
-                            console.log('usename: connect is  fail!')
-                        };
-                    });
-                }else{
-                    resolve(this.dbClient)
-                };
-            });
-        }else{
-            return new Promise((resolve,reject)=>{
-                if(!this.dbClient){
-                MongoClient.connect(`mongodb://${dataConfig.address}:${dataConfig.port}/${dataConfig.database}`,{ useNewUrlParser: true,useUnifiedTopology: true},
-                (err,client)=>{
-                 if(!err){
-                     this.dbClient=client.db(dataConfig.database);
-                     resolve(this.dbClient)
-                     console.log('nousername:connect is  success!')
-                 }else{
-                     reject(err)
-                     console.log('nousername:connect is  fail!')
-                 };
+    connect() {
+            if (state) {
+                return new Promise((resolve, reject) => {
+                    if (!this.dbClient) {
+                        MongoClient.connect(`mongodb://${dataConfig.username}:${dataConfig.password}@${dataConfig.address}:${dataConfig.port}/${dataConfig.database}`, { useNewUrlParser: true, useUnifiedTopology: true },
+                            (err, client) => {
+                                if (!err) {
+                                    this.dbClient = client.db(dataConfig.database);
+                                    resolve(this.dbClient);
+                                    console.log('usename: connect is  success!')
+                                } else {
+                                    reject(err);
+                                    console.log('usename: connect is  fail!')
+                                };
+                            });
+                    } else {
+                        resolve(this.dbClient)
+                    };
                 });
-                }else{
+            } else {
+                return new Promise((resolve, reject) => {
+                    if (!this.dbClient) {
+                        MongoClient.connect(`mongodb://${dataConfig.address}:${dataConfig.port}/${dataConfig.database}`, { useNewUrlParser: true, useUnifiedTopology: true },
+                            (err, client) => {
+                                if (!err) {
+                                    this.dbClient = client.db(dataConfig.database);
+                                    resolve(this.dbClient)
+                                    console.log('nousername:connect is  success!')
+                                } else {
+                                    reject(err)
+                                    console.log('nousername:connect is  fail!')
+                                };
+                            });
+                    } else {
 
-                    resolve(this.dbClient)
-                };
+                        resolve(this.dbClient)
+                    };
 
-            });
+                });
+            }
+
         }
-
-    }
-    //add data in database
-    add(tableName,json){
-        return new Promise((resovle,reject)=>{
-            this.connect().then(db=>{
-                db.collection(tableName).insertOne(json,(err,result)=>{
-                    if(!err){
+        //add data in database
+    add(tableName, json) {
+        return new Promise((resovle, reject) => {
+            this.connect().then(db => {
+                db.collection(tableName).insertOne(json, (err, result) => {
+                    if (!err) {
 
                         resovle(result);
                         return;
@@ -87,11 +87,11 @@ if(dataConfig.username!=''&& dataConfig.password!=''){
         })
     };
     //delete data
-    dedata(tableName,json){
-        return new Promise((resolve,reject)=>{
-            this.connect().then(db=>{
-                db.collection(tableName).removeOne(json,(err,result)=>{
-                    if(!err){
+    dedata(tableName, json) {
+        return new Promise((resolve, reject) => {
+            this.connect().then(db => {
+                db.collection(tableName).removeOne(json, (err, result) => {
+                    if (!err) {
                         resolve(result);
                         return;
                     };
@@ -101,37 +101,37 @@ if(dataConfig.username!=''&& dataConfig.password!=''){
         });
     };
     //updata data
-    updata(tableName,condition,json){
-      return new Promise((resolve,reject)=>{
-        this.connect().then(db=>{
-            db.collection(tableName).updataOne(condition,{$set:json},(err,result)=>{
-                if(!err){
-                    resolve(result);
-                    return;
-                };
-                reject(err);
+    updata(tableName, condition, json) {
+        return new Promise((resolve, reject) => {
+            this.connect().then(db => {
+                db.collection(tableName).updataOne(condition, { $set: json }, (err, result) => {
+                    if (!err) {
+                        resolve(result);
+                        return;
+                    };
+                    reject(err);
 
-            })
+                })
+            });
         });
-      });
     };
 
     //query
-    find(tableName,json){
-       return new Promise((resolve,reject)=>{
-           this.connect().then(db=>{
-               let result =db.collection(tableName).find(json);
-               result.toArray((err,data)=>{//NOTE:toArray() is mothod of mongodb, not JS
-                  if(!err){
-                      resolve(data);
-                      return;
-                  }
-                  reject(err);
-               });
-           });
-       });
+    find(tableName, json) {
+        return new Promise((resolve, reject) => {
+            this.connect().then(db => {
+                let result = db.collection(tableName).find(json);
+                result.toArray((err, data) => { //NOTE:toArray() is mothod of mongodb, not JS
+                    if (!err) {
+                        resolve(data);
+                        return;
+                    }
+                    reject(err);
+                });
+            });
+        });
     };
 
- }
-  cData=cData.getInstance();
- export {cData}
+}
+cData = cData.getInstance();
+export { cData }
