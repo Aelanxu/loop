9806e650e91dae75aa7cafd6b19f7610a289f657
Aelanxu <aelanxu@outlook.com>
2019-09-26 00:54:49 +0800

    111

--- servers/index.js
+++ servers/index.js
@@ -5,7 +5,7 @@ let app = loop();
 app.get('/', function(req, res) {
     res.end('You send GET request')
 })
-app.get('/name', function(req, res) {
+app.post('/name', function(req, res) {
         res.end('You send POST request')
     })
     //     // 处理PUT请求
