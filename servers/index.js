3366c65dc73e6bb5a7e056c9aa7908d7b73b7ff8
Aelanxu <aelanxu@outlook.com>
2019-09-27 19:32:38 +0800

    444

--- servers/index.js
+++ servers/index.js
@@ -1,33 +1,33 @@
 import loop from './server.js';
-//import { login } from './API/contorl.js';
+import { login } from './API/contorl.js';
 let app = loop();
-app.get('/foo', function handle1(req, res, next) {
-    next(new Error('Bang!'))
-}, function handle2(req, res, next) {
-    res.end('Will not go here')
-}, function handle3(err, req, res, next) {
-    console.log(`Error Caught! Error message is ${err.message}`)
-    next(err)
-})
+// app.get('/foo', function handle1(req, res, next) {
+//     next(new Error('Bang!'))
+// }, function handle2(req, res, next) {
+//     res.end('Will not go here')
+// }, function handle3(err, req, res, next) {
+//     console.log(`Error Caught! Error message is ${err.message}`)
+//     next(err)
+// })
 
-app.get('/foo', function(req, res, next) {
-    res.end('Will not go here too')
-})
+// app.get('/foo', function(req, res, next) {
+//     res.end('Will not go here too')
+// })
 
-app.use('/foo', function(req, res, next) {
-    res.end('Will not go here too')
-})
+// app.use('/foo', function(req, res, next) {
+//     res.end('Will not go here too')
+// })
 
-app.get('/foo', function(err, req, res, next) {
-    console.log(err.name)
-    res.end('Will not go here too')
-})
+// app.get('/foo', function(err, req, res, next) {
+//     console.log(err.name)
+//     res.end('Will not go here too')
+// })
 
-app.use('/foo', function(err, req, res, next) {
-    console.log(`Error Caught! Error message is ${err.message}`)
-    res.end('Go here')
-})
+// app.use('/foo', function(err, req, res, next) {
+//     console.log(`Error Caught! Error message is ${err.message}`)
+//     res.end('Go here')
+// })
 
-
+app.get('/foo', login)
 
 app.listen(8888)
