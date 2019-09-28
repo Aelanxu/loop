82f5e47e6c2df2156ec8d16006d27a3c3f7b7507
Aelanxu <aelanxu@outlook.com>
2019-09-28 23:59:37 +0800

    121

--- servers/zero/response.js
+++ servers/zero/response.js
@@ -5,37 +5,37 @@ let res = Object.create(http.ServerResponse.prototype)
 res.send = function(values) {
         //check arguments 
         let body = values
-            // if (arguments.length === 2) {
-            //     if (typeof arguments[0] !== 'number' && typeof arguments[1] === 'number') {
-            //         this.statusCode = arguments[1]
-            //         this.writeHead(200, { 'Content-Type': 'text/plain;charset = utf-8 ' })
-            //         body = '*need the first argument is Number!*'
-            //         this.end(body)
+        if (arguments.length === 2) {
+            if (typeof arguments[0] !== 'number' && typeof arguments[1] === 'number') {
+                this.statusCode = arguments[1]
+                this.writeHead(200, { 'Content-Type': 'text/plain;charset = utf-8 ' })
+                body = '*need the first argument is Number!*'
+                this.end(body)
 
-        //     } else {
-        //         this.statusCode = arguments[0]
-        //         this.writeHead(this.statusCode, { 'Content-Type': 'text/html;charset =utf-8' })
-        //         body = arguments[1]
-        //         this.end(body)
+            } else {
+                this.statusCode = arguments[0]
+                this.writeHead(this.statusCode, { 'Content-Type': 'text/html;charset =utf-8' })
+                body = arguments[1]
+                this.end(body)
 
-        //     }
-        //     if (typeof arguments[0] === 'number' && arguments.length === 1) {
-        //         this.writeHead(200, { 'Content-Type': 'text/plain;charset = utf-8' })
+            }
+            if (typeof arguments[0] === 'number' && arguments.length === 1) {
+                this.writeHead(200, { 'Content-Type': 'text/plain;charset = utf-8' })
 
-        //         body = arguments[0]
-
-
-        //     }
+                body = arguments[0]
 
 
-        // }
-        // if (req.fresh) this.statusCode = 304;
-        // if (204 === this.statusCode || 304 === this.statusCode) {
-        //     this.removeHeader('Content-Type');
-        //     this.removeHeader('Content-Length');
-        //     this.removeHeader('Transfer-Encoding');
-        //     body = '';
-        // }
+            }
+
+
+        }
+        if (req.fresh) this.statusCode = 304;
+        if (204 === this.statusCode || 304 === this.statusCode) {
+            this.removeHeader('Content-Type');
+            this.removeHeader('Content-Length');
+            this.removeHeader('Transfer-Encoding');
+            body = '';
+        }
         this.writeHead(200, { 'Content-Type': 'text/plain' })
         this.end(body)
         return this
