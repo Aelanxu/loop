6da20d8e2f65ee4e9f132d8e3d908a1e7e1d25c1
Aelanxu <aelanxu@outlook.com>
2019-09-28 23:51:10 +0800

    4545

--- servers/index.js
+++ servers/index.js
@@ -4,14 +4,14 @@ import { login } from './API/contorl.js';
 const app = loop();
 
 
-    // app.get('/foo', function handle1(req, res, next) {
-    //     next(new Error('Bang!'))
-    // }, function handle2(req, res, next) {
-    //     res.end('Will not go here')
-    // }, function handle3(err, req, res, next) {
-    //     console.log(`Error Caught! Error message is ${err.message}`)
-    //     next(err)
-    // })
+// app.get('/foo', function handle1(req, res, next) {
+//     next(new Error('Bang!'))
+// }, function handle2(req, res, next) {
+//     res.end('Will not go here')
+// }, function handle3(err, req, res, next) {
+//     console.log(`Error Caught! Error message is ${err.message}`)
+//     next(err)
+// })
 
 // app.get('/foo', function(req, res, next) {
 //     res.end('Will not go here too')
@@ -31,11 +31,11 @@ const app = loop();
 //     res.end('Go here')
 // })
 
-app.get('/foo', function(req, res) {  
-  // res.end('222')
-   res.send('11')
- console.log(res.send)
-    
+app.get('/foo', function(req, res, next) {
+    // res.end('222')
+    res.send('eeee')
+
+
 
 
 })
