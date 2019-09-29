6394e61896ae4dfc3143b3fe22f2aea66e4ed794
Aelanxu <aelanxu@outlook.com>
2019-09-29 15:13:54 +0800

    1111

--- servers/zero/response.js
+++ servers/zero/response.js
@@ -1,12 +1,1 @@
-'use strict';
-import * as http from 'http'
-let res = Object.create(http.ServerResponse.prototype)
-
-    res.send=function(body){
-        this.writeHead(200,{'Content-Type': 'text/plain;charset=utf-8'})
-        this.end(body)
-        return this
-    }
- 
-
-export default res
+qw
