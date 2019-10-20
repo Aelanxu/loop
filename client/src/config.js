a1d6e70f3ad35cf44ef67ed2d10f61e4a6ebd9bd
Aelanxu <aelanxu@outlook.com>
2019-10-20 22:22:29 +0800

    1111

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
-const host = "http://127.0.0.1";
-//const host = "http://106.54.131.66"
+//const host = "http://127.0.0.1";
+const host = "http://106.54.131.66"
+
 function localDate(t = Date.now()) {
     const d = new Date(t);
     d.setMinutes(d.getMinutes() - d.getTimezoneOffset());
