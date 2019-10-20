0b230c7691c9b714c2f468b552fac5b2fc356b69
Aelanxu <aelanxu@outlook.com>
2019-10-20 19:51:59 +0800

    1212

--- servers/index.js
+++ servers/index.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import * as bodyParser from 'body-parser'
-import { controlers } from './API/contorl.js';
+import { controlers } from './API/contorl.js'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+const __filename = fileURLToPath(
+    import.meta.url)
+const __dirname = dirname(__filename)
 const app = express();
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
@@ -10,6 +15,7 @@ app.all('*', function(req, res, next) {
     res.header("Content-Type", "application/json;charset=utf-8")
     next();
 });
+app.use(express.static(join(__dirname, '../dist')));
 //under have been to accept json config
 app.use(bodyParser.default.json())
 app.use(bodyParser.default.urlencoded({ extended: false })) //tip:resolve accept the data of post is undefind
@@ -20,4 +26,4 @@ app.post('/read', controlers.read)
 app.post('/updata', controlers.update)
 
 
-app.listen(8888)
+app.listen(80)
